6c298be9912ed6152535cc22974e302c3bf5b86a
Claudio Ortolina <claudio@fullyforged.com>
2017-07-31 17:29:42 +0300

    Adds Mbrainz api client (can fetch artist info)

---
+++ lib/gig/mbrainz/api_client.ex
@@ -0,0 +1,33 @@
+defmodule Gig.Mbrainz.ApiClient do
+  @moduledoc """
+  This module includes low-level functions to access the MusicBrainz
+  api and return raw data (in form of binary maps).
+  """
+
+  @base_url "https://musicbrainz.org/ws/2"
+  @user_agent Application.get_env(:gig, :mbrainz_user_agent)
+  @headers ["User-Agent": @user_agent]
+
+  alias HTTPotion.{Response,
+                   ErrorResponse}
+
+  @type mbid :: String.t
+
+  def get_artist(mbid) do
+    path = "/artist/#{mbid}"
+    params = %{inc: "release-groups", fmt: "json"}
+
+    do_get(path, params)
+  end
+
+  def do_get(path, params) do
+    case HTTPotion.get(@base_url <> path, query: params, headers: @headers) do
+      %Response{status_code: 200, body: body} ->
+        Poison.decode(body)
+      %Response{status_code: status_code, body: body} ->
+        {:error, {status_code, body}}
+      %ErrorResponse{message: message} ->
+        message
+    end
+  end
+end
